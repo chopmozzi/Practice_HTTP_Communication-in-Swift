4f97000b601bc80bfc222a98aaa399f25bebfbeb
chopmozzi <chopmojji@gmail.com>
2023-01-17 01:42:52 +0900

    Initial Commit

---
+++ HTTP Communication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HTTP Communication
+//
+//  Created by 황지웅 on 2023/01/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
